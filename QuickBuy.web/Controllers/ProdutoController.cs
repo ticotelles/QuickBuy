9e3b9f4a32cf73304d2cbddb580286330787f79d
Arthur Telles <arthurtelles19@gmail.com>
2023-05-24 20:07:05 -0300

    Adicionando metodos API

---
+++ QuickBuy.web/Controllers/ProdutoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+
+namespace QuickBuy.web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+    }
+}
