bfafd131b6a0e21b51c489c9670acf8ea82b476a
Arthur Telles <arthurtelles19@gmail.com>
2023-06-02 15:26:51 -0300

    começando a criar os componentes em angular

--- QuickBuy.web/Controllers/ProdutoController.cs
+++ QuickBuy.web/Controllers/ProdutoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+using System;
 
 namespace QuickBuy.web.Controllers
 {
@@ -10,6 +12,35 @@ namespace QuickBuy.web.Controllers
         public ProdutoController(IProdutoRepositorio produtoRepositorio)
         {
             _produtoRepositorio = produtoRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto)
+        {
+            try
+            {
+                
+                _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
         }
     }
 }
