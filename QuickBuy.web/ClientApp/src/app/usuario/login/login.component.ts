64048e01ae791d548f12f027daeb651264c199c9
Arthur Telles <arthurtelles19@gmail.com>
2023-06-30 15:01:18 -0300

    controlando acesso com guarda rotas

--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { ActivatedRoute, Router } from "@angular/router";
 
 
 @Component({
@@ -10,18 +11,19 @@ import { Usuario } from "../../modelo/usuario";
 export class LoginComponent {
 
   public usuario;
-  
+  public returnUrl: string;
 
-
-  constructor() {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) { 
     this.usuario = new Usuario();
+    this.returnUrl= this.activatedRouter.snapshot.queryParams['returnUrl']
   }
 
- /* entrar() {
+  entrar() {
     if (this.usuario.email == "tico@teste.com" && this.usuario.senha == "123123") {
-      this.usuarioAutenticado = true;
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
     }
     
   }
-  */
+  
 }
